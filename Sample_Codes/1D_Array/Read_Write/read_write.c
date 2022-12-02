29c238fc98fce8f1c8dc545f4de71b62e0244500
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2022-12-02 15:53:56 +0530

    creating a sample 1D array code

---
+++ Sample_Codes/1D_Array/Read_Write/read_write.c
@@ -0,0 +1,22 @@
+//* How to read & display the values of an array and store it
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[50], n;                           // Declaration of 'arr'
+    printf("Enter number of elements:\n>> "); // No of elements
+    scanf("%d", &n);                          // Reading the limit into n
+    printf("\nEnter the elements:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]); // Reading n elements
+    }
+    printf("\n-- Elements of the array are:\n>> ");
+    for (int j = 0; j < n; j++) // Displaying n elements
+    {
+        printf("%d", arr[j]);
+        printf("\t");
+    }
+    return 0;
+}
