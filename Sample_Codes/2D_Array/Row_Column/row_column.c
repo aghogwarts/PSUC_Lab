376461c8fe4132f41e88ae0e33dc546c4986edd0
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2022-12-02 19:42:33 +0530

    matrix problems

--- Sample_Codes/2D_Array/Row_Column/row_column.c
+++ Sample_Codes/2D_Array/Row_Column/row_column.c
@@ -1,4 +1,4 @@
-//* Read a matrix and display it
+//* Finding the row and column sum of a matrix
 
 #include <stdio.h>
 
@@ -7,16 +7,27 @@ int main()
     int i, j, m, n, a[50][50];
     printf("\nEnter dimensions of the Matrix:\n>> ");
     scanf("%d,%d", &m, &n);
+    int row_sum[m], column_sum[n];
     printf("\nEnter the elements:");
     for (i = 0; i < m; i++)
     {
+        row_sum[i] = 0;
         printf("\n-- Enter elements of Row %d:\n", i + 1);
         for (j = 0; j < n; j++)
         {
             scanf("%d", &a[i][j]);
+            row_sum[i] += a[i][j];
         }
     }
-    printf("\n\n>> The entered Matrix is:\n\n");
+    for (j = 0; j < n; j++)
+    {
+        column_sum[j] = 0;
+        for (i = 0; i < m; i++)
+        {
+            column_sum[j] += a[i][j];
+        }
+    }
+    printf("\n\n>> The Calculated Matrix is:\n\n");
     for (i = 0; i < m; i++)
     {
         printf("|  ");
@@ -24,7 +35,12 @@ int main()
         {
             printf("%d  ", a[i][j]);
         }
-        printf("|\n");
+        printf("| = %d\n", row_sum[i]);
+    }
+    printf("   ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d  ", column_sum[i]);
     }
     printf("\n");
     return 0;
