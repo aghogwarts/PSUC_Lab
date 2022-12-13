dd1929bc57e1c5bf295c8dbd8485c8f8f368813a
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2022-12-13 16:31:24 +0530

    complete Lab_3 main exercises

--- Lab/Lab_3/3. Armstrong/armstrong.c
+++ Lab/Lab_3/3. Armstrong/armstrong.c
@@ -2,23 +2,24 @@
 
 int main()
 {
-    int temp, num, r, sum=0;
-    printf("Enter number to check if it's an Armstrong: ");
+    int temp, num, r, sum = 0;
+    printf("\nEnter number to check if it's an Armstrong:\n>> ");
     scanf("%d", &num);
     temp = num;
     while (temp != 0)
     {
         r = temp % 10;
-        sum = sum + (r*r*r);
+        sum = sum + (r * r * r);
         temp = temp / 10;
     }
     if (num == sum)
     {
-        printf("It is an armstrong");
+        printf("\n-- It is an armstrong");
     }
     else
     {
-        printf("It is not an armstrong");
+        printf("\n-- It is not an armstrong");
     }
+    printf("\n");
     return 0;
 }
--- Lab/Lab_3/4. Count_prime_comp/count_prime_comp.c
+++ Lab/Lab_3/4. Count_prime_comp/count_prime_comp.c
@@ -2,41 +2,40 @@
 
 int main()
 {
-    int n, i, prime=1, prime_num=0, comp_num=0;
+    int n, i, prime = 1, prime_num = 0, comp_num = 0;
     do
     {
-        printf("Enter a number: ");
+        printf("\nEnter a number:\n>> ");
         scanf("%d", &n);
         i = 2;
         prime = 1;
-        if(n==-1)
+        if (n == -1)
         {
             break;
         }
-        if(n==1)
+        if (n == 1)
         {
-            prime=3;
+            prime = 3;
         }
-        while(i<(n/2))
+        while (i < (n / 2))
         {
-            if(n%i==0)
+            if (n % i == 0)
             {
-                prime=0;
+                prime = 0;
                 break;
             }
             i++;
         }
-        if(prime==1)
+        if (prime == 1)
         {
             prime_num++;
         }
-        else if(prime==0)
+        else if (prime == 0)
         {
             comp_num++;
         }
-    }
-    while(n != -1);
-    printf("No of prime numbers is: %d\n", prime_num);
-    printf("No of composite numbers is: %d\n", comp_num);
+    } while (n != -1);
+    printf("\n-- No of prime numbers is: %d", prime_num);
+    printf("\n-- No of composite numbers is: %d\n", comp_num);
     return 0;
 }
