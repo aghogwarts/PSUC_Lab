0d7bfcebf5b0328c7d8c912c870edd53257cafc0
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2022-12-14 00:42:09 +0530

    update Lab_5

--- Lab/Lab_5/2. Bubble_Sort/bubble_sort.c
+++ Lab/Lab_5/2. Bubble_Sort/bubble_sort.c
@@ -1,59 +1,60 @@
-#include  <stdio.h>
+#include <stdio.h>
 
 int main()
 {
     int i = 0, j = 0, n, temp = 0;
     char order;
-    printf("Enter choice: a-for Ascending order, d-for Descending order\n");
+    printf("\nEnter choice: a-for Ascending order, d-for Descending order:\n>> ");
     scanf("%c", &order);
-    printf("\nEnter the number of elements in the array: ");
+    printf("\nEnter the number of elements in the array:\n>> ");
     scanf("%d", &n);
     int A[n];
-    printf("Enter array elements:\n");
-    for (i = 0; i<n;i++)
+    printf("\nEnter array elements:\n");
+    for (i = 0; i < n; i++)
     {
         scanf("%d", &A[i]);
     }
     switch (order)
     {
     case 'a':
-        for (i = 0; i<n-1;i++)
+        for (i = 0; i < n - 1; i++)
         {
-            for (j = 0; j<n-i-1;j++)
+            for (j = 0; j < n - i - 1; j++)
             {
-                if (A[j]>A[j+1])
+                if (A[j] > A[j + 1])
                 {
                     temp = A[j];
-                    A[j] = A[j+1];
-                    A[j+1] = temp;
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
                 }
             }
         }
-        printf("\nThe array in Ascending order is:\n");
-        for (i = 0; i<n; i++)
+        printf("\n-- The array in Ascending order is:\n>> ");
+        for (i = 0; i < n; i++)
         {
-            printf("%d\t",A[i]);
+            printf("%d  ", A[i]);
         }
         break;
     case 'd':
-        for (i = 0; i<n-1;i++)
+        for (i = 0; i < n - 1; i++)
         {
-            for (j = 0; j<n-i-1;j++)
+            for (j = 0; j < n - i - 1; j++)
             {
-                if (A[j]<A[j+1])
+                if (A[j] < A[j + 1])
                 {
                     temp = A[j];
-                    A[j] = A[j+1];
-                    A[j+1] = temp;
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
                 }
             }
         }
-        printf("\nThe array in Descending order is:\n");
-        for (i = 0; i<n; i++)
+        printf("\n-- The array in Descending order is:\n>> ");
+        for (i = 0; i < n; i++)
         {
-            printf("%d\t",A[i]);
+            printf("%d  ", A[i]);
         }
         break;
     }
+    printf("\n\n");
     return 0;
 }
